52261ffd8b53705107f4da4c115abca68ba3e84c
vinhtran6686 <vinhtran.6686@gmail.com>
2024-08-05 15:07:56 +0700

    chore: Add newDiv element with red text and font size to .root element

--- simpleScript.js
+++ simpleScript.js
@@ -1,3 +1,14 @@
-// simpleScript.js
 console.log("Simple JS script has been loaded and run!");
 alert("Simple JS script has been loaded and run!");
+
+// Tạo một phần tử div mới
+var newDiv = document.createElement('div');
+newDiv.innerText = 'Đây là phần tử mới được thêm vào!'; // Thiết lập nội dung text cho div
+newDiv.style.color = 'red'; // Đặt màu chữ là đỏ cho div
+newDiv.style.fontSize = '20px'; // Đặt kích thước font cho div
+
+// Lấy phần tử .root đầu tiên trong tài liệu
+var parentNode = document.querySelector('.root');
+
+// Thêm phần tử div mới vào trong phần tử .root
+parentNode.appendChild(newDiv);
